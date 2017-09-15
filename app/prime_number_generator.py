ea78b13335f94c9bf26c34baf5255801925a1b06
Patrick Python Book <pwalukagga@gmail.com>
2017-09-15 12:06:10 +0300

    app package for prime number generator

---
+++ app/prime_number_generator.py
@@ -0,0 +1,10 @@
+def generate_primes(lower_limit, upper_limit):
+    """Function to generate prime numbers from a given interval
+    param lower_limit: int variable to store lower limit value
+    param upper_limit: int variable to store  upper limit value
+    return: list of prime numbers
+    """
+
+    if not isinstance(lower_limit, int) or not isinstance(upper_limit, 
+      int):
+        raise ValueError("Both interval values must be integers")  
